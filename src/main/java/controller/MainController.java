4c5f55aa79e38312e4c24cb83966e58142b0543e
Akil1928 <akilwatsonch1928@gmail.com>
2026-03-25 18:04:38 -0600

    akil

--- src/main/java/controller/MainController.java
+++ src/main/java/controller/MainController.java
@@ -1,93 +1,63 @@
 package controller;
 
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
-import javafx.scene.control.ScrollPane;
+import javafx.scene.control.ProgressBar;
 import javafx.scene.control.Slider;
 import javafx.scene.control.TabPane;
+import javafx.scene.control.TextField;
 
-import java.net.URL;
-import java.util.ResourceBundle;
+public class MainController {
 
-public class MainController implements Initializable {
+    @FXML
+    private Canvas binCanvas;
 
-    @FXML private TabPane mainTabPane;
-    @FXML private ScrollPane scrollFactTree;
-    @FXML private Canvas canvasTree;
+    @FXML
+    private Button btnBinAnimate;
 
-    @FXML private Slider sliderFactN;
-    @FXML private Label lblFactN;
-    @FXML private Button btnFactReset;
-    @FXML private Button btnFactCalc;
-    @FXML private Label lblComplexity;
-    @FXML private Label lblFactCalls;
-    @FXML private Label lblFactResult;
-    @FXML private ListView<String> listSteps;
+    @FXML
+    private Button btnBinClear;
 
-    @Override
-    public void initialize(URL url, ResourceBundle resourceBundle) {
-        setupFactTab();
-        resetFactTab();
-    }
+    @FXML
+    private Button btnBinGen;
 
-    private void setupFactTab() {
-        sliderFactN.setMin(1);
-        sliderFactN.setMax(12);
-        sliderFactN.setValue(5);
-        sliderFactN.setMajorTickUnit(1);
-        sliderFactN.setMinorTickCount(0);
-        sliderFactN.setSnapToTicks(true);
+    @FXML
+    private Button btnBinSearch;
 
-        lblFactN.setText(String.valueOf((int) sliderFactN.getValue()));
+    @FXML
+    private Label lblBinArray;
 
-        sliderFactN.valueProperty().addListener((obs, oldVal, newVal) ->
-                lblFactN.setText(String.valueOf((int) newVal.doubleValue())));
+    @FXML
+    private Label lblBinComplex;
 
-        btnFactCalc.setOnAction(e -> runFactorial());
-        btnFactReset.setOnAction(e -> resetFactTab());
-    }
+    @FXML
+    private Label lblBinComps;
 
-    private void resetFactTab() {
-        sliderFactN.setValue(5);
-        lblFactN.setText("5");
-        lblComplexity.setText("--");
-        lblFactCalls.setText("--");
-        lblFactResult.setText("--");
-        listSteps.setItems(FXCollections.observableArrayList());
-    }
+    @FXML
+    private Label lblBinResult;
 
-    private void runFactorial() {
-        int n = (int) sliderFactN.getValue();
+    @FXML
+    private Label lblBinSize;
 
-        ObservableList<String> steps = FXCollections.observableArrayList();
-        long[] calls = new long[1];
+    @FXML
+    private Label lblBinTime;
 
-        long result = factorial(n, steps, calls);
+    @FXML
+    private ListView<?> listSteps;
 
-        lblFactResult.setText(String.valueOf(result));
-        lblFactCalls.setText(String.valueOf(calls[0]));
-        lblComplexity.setText("O(n)");
-        listSteps.setItems(steps);
-    }
+    @FXML
+    private TabPane mainTabPane;
 
-    private long factorial(int n, ObservableList<String> steps, long[] calls) {
-        calls[0]++;
-        steps.add("factorial(" + n + ")");
+    @FXML
+    private ProgressBar progressBarBin;
 
-        if (n <= 1) {
-            steps.add("return 1");
-            return 1;
-        }
+    @FXML
+    private Slider sliderBinSize;
 
-        long partial = factorial(n - 1, steps, calls);
-        long result = n * partial;
-        steps.add(n + " * factorial(" + (n - 1) + ") = " + result);
-        return result;
-    }
+    @FXML
+    private TextField txtBinValue;
+
 }
